023842787813b22f8b7c5e929818b6550f378419
sureshishan <bethalasuresh@gmail.com>
2018-07-21 20:04:21 +0530

    BottomUp or Java first Web service with Seurity enabled(UserNameToken profile)

---
+++ javafirstws/src/main/java/com/bharath/ws/soap/config/UTPasswordCallback.java
@@ -0,0 +1,36 @@
+package com.bharath.ws.soap.config;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
+public class UTPasswordCallback implements CallbackHandler {
+
+	Map<String, String> passwords = new HashMap<>();
+
+	public UTPasswordCallback() {
+		passwords.put("suresh", "suresh");
+		passwords.put("ishan", "suresh");
+	}
+
+	@Override
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+
+		for (Callback callback : callbacks) {
+			WSPasswordCallback passwordCallback = (WSPasswordCallback) callback;
+			String password = passwords.get(passwordCallback.getIdentifier());
+			if(password != null) {
+				passwordCallback.setPassword(password);
+				return;
+			}
+		}
+
+	}
+
+}
---
+++ javafirstws/src/main/java/com/bharath/ws/soap/config/WebServiceConfig.java
@@ -0,0 +1,39 @@
+package com.bharath.ws.soap.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.ws.Endpoint;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.wss4j.common.ConfigurationConstants;
+import org.apache.wss4j.dom.WSConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.bharath.ws.soap.PaymentProcessorImpl;
+
+@Configuration
+public class WebServiceConfig {
+
+	@Autowired
+	private Bus bus;
+
+	@Bean
+	public Endpoint endpoint() {
+		EndpointImpl endpoint = new EndpointImpl(bus, new PaymentProcessorImpl());
+		endpoint.publish("/paymentProcessor");
+
+		Map<String, Object> propsIn = new HashMap<>();
+		propsIn.put(ConfigurationConstants.ACTION, ConfigurationConstants.USERNAME_TOKEN);
+		propsIn.put(ConfigurationConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
+		propsIn.put(ConfigurationConstants.PW_CALLBACK_CLASS, UTPasswordCallback.class.getName());
+		WSS4JInInterceptor wssIn = new WSS4JInInterceptor(propsIn);
+		endpoint.getInInterceptors().add(wssIn);
+		return endpoint;
+	}
+
+}
