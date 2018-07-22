53a5ebf16c0f4cf4de3ae2a100dffc8e23de2dac
sureshishan <bethalasuresh@gmail.com>
2018-07-22 12:47:51 +0530

    soap fault enabled for javafirstws

--- javafirstws/src/main/java/com/bharath/ws/soap/PaymentProcessor.java
+++ javafirstws/src/main/java/com/bharath/ws/soap/PaymentProcessor.java
@@ -7,11 +7,13 @@ import javax.jws.WebService;
 
 import com.bharath.ws.soap.dto.PaymentProcessorRequest;
 import com.bharath.ws.soap.dto.PaymentProcessorResponse;
+import com.bharath.ws.soap.exception.ServiceException;
 
 @WebService(name = "PaymentProcessor")
 public interface PaymentProcessor {
 
 	@WebMethod
 	public @WebResult(name = "response") PaymentProcessorResponse processPayment(
-			@WebParam(name = "paymentProcessorRequest") PaymentProcessorRequest paymentProcessorRequest);
+			@WebParam(name = "paymentProcessorRequest") PaymentProcessorRequest paymentProcessorRequest)
+			throws ServiceException;
 }
--- javafirstws/src/main/java/com/bharath/ws/soap/PaymentProcessorImpl.java
+++ javafirstws/src/main/java/com/bharath/ws/soap/PaymentProcessorImpl.java
@@ -2,10 +2,15 @@ package com.bharath.ws.soap;
 
 import com.bharath.ws.soap.dto.PaymentProcessorRequest;
 import com.bharath.ws.soap.dto.PaymentProcessorResponse;
+import com.bharath.ws.soap.exception.ServiceException;
 
 public class PaymentProcessorImpl implements PaymentProcessor {
 
-	public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest) {
+	public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest) throws ServiceException{
+		if (paymentProcessorRequest.getCreditCardInfo().getCardNumber() == null
+				|| paymentProcessorRequest.getCreditCardInfo().getCardNumber().length() == 0) {
+			throw new ServiceException("Invalid card number");
+		}
 		PaymentProcessorResponse paymentProcessorResponse = new PaymentProcessorResponse();
 		// Business Logic or a call to a Business Logic Class Goes Here.
 		paymentProcessorResponse.setResult(true);
